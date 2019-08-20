2996c305593dfc5c80a3b8ec9d812936dd6ea018
m-a-h-e <maik.heese@web.de>
2019-08-20 20:51:48 +0200

    flat layer import

--- numpy_neural_network/__init__.py
+++ numpy_neural_network/__init__.py
@@ -1,7 +1,8 @@
 
-import numpy_neural_network.conn_layer
-import numpy_neural_network.func_layer
-import numpy_neural_network.loss_layer
+from numpy_neural_network.conn_layer import *
+from numpy_neural_network.func_layer import *
+from numpy_neural_network.loss_layer import *
+
 import numpy_neural_network.optimizer
 import numpy_neural_network.network
 
