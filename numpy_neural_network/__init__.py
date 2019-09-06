a34ffd37aef1e147ef0982254d5662ca800a8e35
m-a-h-e <maik.heese@web.de>
2019-09-06 19:25:56 +0200

    added convolutional and max pooling classes

--- numpy_neural_network/__init__.py
+++ numpy_neural_network/__init__.py
@@ -2,6 +2,8 @@
 from numpy_neural_network.conn_layer import *
 from numpy_neural_network.func_layer import *
 from numpy_neural_network.loss_layer import *
+from numpy_neural_network.conv_layer import *
+from numpy_neural_network.pool_layer import *
 
 import numpy_neural_network.optimizer
 import numpy_neural_network.network
