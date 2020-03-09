2d82ac0fd3261d9d403cb8342980016f873a810c
maideas <maik.heese@web.de>
2020-03-10 00:02:40 +0100

    created mp4 from non-linear regression using Tanh network

--- examples/non_linear_regression_tanh/example.py
+++ examples/non_linear_regression_tanh/example.py
@@ -35,12 +35,7 @@ model = npnn.network.Model([
 
 model.loss_layer = npnn.loss_layer.RMSLoss(1)
 
-#optimizer = npnn.optimizer.SGD(model, alpha=1e-2)  # ReLU seems to need a little bit larger alpha
-#optimizer = npnn.optimizer.SGD(model, alpha=2e-3)  # Tanh
-#optimizer = npnn.optimizer.RMSprop(model, alpha=5e-5)  # ReLU
-#optimizer = npnn.optimizer.Adam(model, alpha=5e-3)  # Sigmoid
-optimizer = npnn.optimizer.Adam(model, alpha=2e-4)  # Tanh
-#optimizer = npnn.optimizer.Adam(model, alpha=1e-3)  # LeakyReLU
+optimizer = npnn.optimizer.Adam(model, alpha=5e-4)
 
 optimizer.dataset = npnn_datasets.NoisySine()
 
@@ -54,30 +49,47 @@ ax2 = plt.subplot(212)
 
 loss_x = []
 train_loss_y = []
+validation_loss_y = []
 
 for episode in np.arange(1000):
 
-    optimizer.step()
-    print("episide = {0:5d}, alpha = {1:8.6f}, loss = {2:8.6f}".format(episode, optimizer.alpha, np.mean(optimizer.loss)))
-
-    ax1.cla()
-    ax1.scatter(optimizer.dataset.x_data, optimizer.dataset.y_data, s=2, c='tab:green')
-
+    # plot the green dataset points ...
     x = optimizer.dataset.x_data
     z = optimizer.predict(x)
+    ax1.cla()
+    ax1.scatter(optimizer.dataset.x_data, optimizer.dataset.y_data, s=2, c='tab:green')
     ax1.plot(x, z)
 
-    loss_x.append(episode)
-    train_loss_y.append(np.mean(optimizer.loss))
+    # step the optimizer ...
+    optimizer.step()
 
+    # append the optimizer step train loss ...
+    loss_x.append(episode)
+    tloss = np.mean(optimizer.loss)
+    train_loss_y.append(tloss)
+
+    # calculate and append the validation loss ...
+    vloss = np.mean(
+        optimizer.calculate_loss(
+            optimizer.dataset.x_validation_data,
+            optimizer.dataset.y_validation_data
+        )
+    )
+    validation_loss_y.append(vloss)
+
+    # print the episode and loss values ...
+    print("episode = {0:5d}, tloss = {2:8.6f}, vloss = {2:8.6f}".format(episode, tloss, vloss))
+
+    # print the train loss (blue) and validation loss (orange) ...
     ax2.cla()
-    ax2.set_ylim(0, 0.1+max(train_loss_y))
     ax2.set_xlabel('episode')
     ax2.set_ylabel('loss')
-    ax2.plot(loss_x, train_loss_y)
+    ax2.set_yscale('log')
+    ax2.set_ylim((min(train_loss_y)/2.0, max(train_loss_y)*2.0))
+    ax2.plot(loss_x, train_loss_y, loss_x, validation_loss_y)
 
     plt.draw()
-    plt.savefig('video/episode{0:04d}.png'.format(episode))
+    plt.savefig('video/episode{0:04d}.png'.format(episode))  # save png to create mp4 later on
     plt.pause(0.001)
 
 input("Press Enter to close ...")
