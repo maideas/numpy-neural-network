b609d2db349b44d597c4baeec2ac018a663d1c35
maideas <maik.heese@web.de>
2020-03-14 19:29:48 +0100

    intermediate

--- npnn_datasets/__init__.py
+++ npnn_datasets/__init__.py
@@ -1,7 +1,8 @@
 
-from npnn_datasets.dataset         import *
-from npnn_datasets.noisy_sine      import *
-from npnn_datasets.noisy_linear    import *
-from npnn_datasets.xor_function    import *
-from npnn_datasets.xor_two_classes import *
+from npnn_datasets.dataset          import *
+from npnn_datasets.noisy_sine       import *
+from npnn_datasets.noisy_linear     import *
+from npnn_datasets.xor_function     import *
+from npnn_datasets.xor_two_classes  import *
+from npnn_datasets.four_img_classes import *
 
