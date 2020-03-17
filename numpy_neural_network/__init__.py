42fc0635d6141403bee1e509948533c861bfb775
maideas <maik.heese@web.de>
2020-03-17 20:32:45 +0100

    renamed dense layer and added dropout layer

--- numpy_neural_network/__init__.py
+++ numpy_neural_network/__init__.py
@@ -1,9 +1,10 @@
 
-from numpy_neural_network.conn_layer import *
-from numpy_neural_network.func_layer import *
-from numpy_neural_network.loss_layer import *
-from numpy_neural_network.conv_layer import *
-from numpy_neural_network.pool_layer import *
+from numpy_neural_network.dense_layer   import *
+from numpy_neural_network.func_layer    import *
+from numpy_neural_network.loss_layer    import *
+from numpy_neural_network.conv_layer    import *
+from numpy_neural_network.pool_layer    import *
+from numpy_neural_network.dropout_layer import *
 
 import numpy_neural_network.optimizer
 import numpy_neural_network.network
