f6745d8992881c43c52d3ed253516543a09ac374
maideas <maik.heese@web.de>
2020-03-17 20:36:01 +0100

    added XOR binary classifier and MNIST datasets

--- npnn_datasets/__init__.py
+++ npnn_datasets/__init__.py
@@ -1,8 +1,10 @@
 
-from npnn_datasets.dataset          import *
-from npnn_datasets.noisy_sine       import *
-from npnn_datasets.noisy_linear     import *
-from npnn_datasets.xor_function     import *
-from npnn_datasets.xor_two_classes  import *
-from npnn_datasets.four_img_classes import *
+from npnn_datasets.dataset               import *
+from npnn_datasets.noisy_sine            import *
+from npnn_datasets.noisy_linear          import *
+from npnn_datasets.xor_function          import *
+from npnn_datasets.xor_two_classes       import *
+from npnn_datasets.xor_binary_classifier import *
+from npnn_datasets.four_img_classes      import *
+from npnn_datasets.mnist                 import *
 
