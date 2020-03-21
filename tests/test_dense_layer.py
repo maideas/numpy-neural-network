74a268f9ecae6701468a0e9fb87054fcca024a9c
maideas <maik.heese@web.de>
2020-03-21 16:35:08 +0100

    adapted dense layer test to check 3D data handling

--- tests/test_dense_layer.py
+++ tests/test_dense_layer.py
@@ -7,71 +7,95 @@ import numpy as np
 from numpy_neural_network import Dense
 
 
-def ref_forward(x, w, size_in, size_out):
+def ref_forward(x, w, shape_in, shape_out):
 
     # initialization of the output vector with zeros ...
-    y = np.zeros(size_out)
+    y = np.zeros(shape_out)
 
     # traverse output ...
-    for y_index in np.arange(size_out):
+    for y_h_index in np.arange(shape_out[0]):
+        for y_w_index in np.arange(shape_out[1]):
+            for y_d_index in np.arange(shape_out[2]):
 
-        # traverse input space related to the output index ...
-        for x_index in np.arange(size_in):
+                w_out_index = y_h_index * shape_out[1] * shape_out[2] + y_w_index * shape_out[2] + y_d_index
 
-            # update the selected output value using the related x and w values ...
-            x_val = x[x_index]
-            w_val = w[y_index, x_index]
+                # traverse input space related to the output index ...
+                for x_h_index in np.arange(shape_in[0]):
+                    for x_w_index in np.arange(shape_in[1]):
+                        for x_d_index in np.arange(shape_in[2]):
 
-            # incremental output value update ...
-            y[y_index] += x_val * w_val
+                            x_val = x[x_h_index, x_w_index, x_d_index]
 
-        # add weighted bias information to the selected output value ...
-        bias_val = w[y_index, -1]
-        y[y_index] += bias_val
+                            w_in_index = x_h_index * shape_in[1] * shape_in[2] + x_w_index * shape_in[2] + x_d_index
+
+                            w_val = w[w_out_index, w_in_index]
+
+                            # incremental output value update ...
+                            y[y_h_index, y_w_index, y_d_index] += x_val * w_val
+
+                # add weighted bias information to the selected output value ...
+                bias_val = w[w_out_index, -1]
+                y[y_h_index, y_w_index, y_d_index] += bias_val
 
     return y
 
 
-def ref_backward_gx(gy, w, size_in, size_out):
+def ref_backward_gx(gy, w, shape_in, shape_out):
 
     # initialization of the output vector with zeros ...
-    gx = np.zeros(size_in)
+    gx = np.zeros(shape_in)
 
-    # traverse output ...
-    for y_index in np.arange(size_out):
+    for y_h_index in np.arange(shape_out[0]):
+        for y_w_index in np.arange(shape_out[1]):
+            for y_d_index in np.arange(shape_out[2]):
 
-        # traverse input space related to the output index ...
-        for x_index in np.arange(size_in):
+                w_out_index = y_h_index * shape_out[1] * shape_out[2] + y_w_index * shape_out[2] + y_d_index
 
-            gy_val = gy[y_index]
-            w_val = w[y_index, x_index]
+                # traverse input space related to the output index ...
+                for x_h_index in np.arange(shape_in[0]):
+                    for x_w_index in np.arange(shape_in[1]):
+                        for x_d_index in np.arange(shape_in[2]):
 
-            # incremental input gradient value update ...
-            gx[x_index] += gy_val * w_val
+                            gy_val = gy[y_h_index, y_w_index, y_d_index]
+
+                            w_in_index = x_h_index * shape_in[1] * shape_in[2] + x_w_index * shape_in[2] + x_d_index
+
+                            w_val = w[w_out_index, w_in_index]
+
+                            gx[x_h_index, x_w_index, x_d_index] += gy_val * w_val
 
     return gx
 
 
-def ref_backward_gw(gy, x, size_in, size_out):
+def ref_backward_gw(gy, x, shape_in, shape_out):
 
     # initialization of the weight gradient array with zeros ...
-    gw = np.zeros((size_out, size_in + 1))
+    gw = np.zeros((np.prod(shape_out), np.prod(shape_in) + 1))
 
     # traverse output ...
-    for y_index in np.arange(size_out):
+    for y_h_index in np.arange(shape_out[0]):
+        for y_w_index in np.arange(shape_out[1]):
+            for y_d_index in np.arange(shape_out[2]):
 
-        # traverse input space related to the output index ...
-        for x_index in np.arange(size_in):
+                w_out_index = y_h_index * shape_out[1] * shape_out[2] + y_w_index * shape_out[2] + y_d_index
 
-             gy_val = gy[y_index]
-             x_val = x[x_index]
+                # traverse input space related to the output index ...
+                for x_h_index in np.arange(shape_in[0]):
+                    for x_w_index in np.arange(shape_in[1]):
+                        for x_d_index in np.arange(shape_in[2]):
 
-             # incremental weight gradient value update ...
-             gw[y_index, x_index] += gy_val * x_val
+                            gy_val = gy[y_h_index, y_w_index, y_d_index]
 
-        # bias weight update ...
-        gy_val = gy[y_index]
-        gw[y_index, -1] += gy_val
+                            x_val = x[x_h_index, x_w_index, x_d_index]
+
+                            w_in_index = x_h_index * shape_in[1] * shape_in[2] + x_w_index * shape_in[2] + x_d_index
+
+                            # incremental weight gradient value update ...
+                            gw[w_out_index, w_in_index] += gy_val * x_val
+
+                # add weighted bias information to the selected output value ...
+                gy_val = gy[y_h_index, y_w_index, y_d_index]
+                gw[w_out_index, -1] += gy_val
 
     return gw
 
@@ -81,44 +105,100 @@ class TestDense(unittest.TestCase):
     def test_dense_layer(self):
 
         # loop over different random layer configurations ...
-        for episode in np.arange(200):
+        for episode in np.arange(100):
 
             if episode == 0:
                 #============================================
-                size_in  = 1
-                size_out = 1
+                h_in = 1
+                w_in = 1
+                d_in = 1
+                h_out = 1
+                w_out = 1
+                d_out = 1
                 #============================================
 
             if episode == 1:
                 #============================================
-                size_in  = 2
-                size_out = 1
+                h_in = 2
+                w_in = 1
+                d_in = 1
+                h_out = 1
+                w_out = 1
+                d_out = 1
                 #============================================
 
             if episode == 2:
                 #============================================
-                size_in  = 1
-                size_out = 2
+                h_in = 1
+                w_in = 2
+                d_in = 1
+                h_out = 1
+                w_out = 1
+                d_out = 1
                 #============================================
 
-            if episode > 2:
+            if episode == 3:
                 #============================================
-                size_in  = np.random.randint(1, 20)
-                size_out = np.random.randint(1, 20)
+                h_in = 1
+                w_in = 1
+                d_in = 2
+                h_out = 1
+                w_out = 1
+                d_out = 1
                 #============================================
 
-            shape_in  = (size_in)
-            shape_out = (size_out)
-            shape_w   = (size_out, size_in + 1)
+            if episode == 4:
+                #============================================
+                h_in = 1
+                w_in = 1
+                d_in = 1
+                h_out = 2
+                w_out = 1
+                d_out = 1
+                #============================================
+
+            if episode == 5:
+                #============================================
+                h_in = 1
+                w_in = 1
+                d_in = 1
+                h_out = 1
+                w_out = 2
+                d_out = 1
+                #============================================
+
+            if episode == 6:
+                #============================================
+                h_in = 1
+                w_in = 1
+                d_in = 1
+                h_out = 1
+                w_out = 1
+                d_out = 2
+                #============================================
+
+            if episode > 6:
+                #============================================
+                h_in  = np.random.randint(1, 10)
+                w_in  = np.random.randint(1, 10)
+                d_in  = np.random.randint(1, 5)
+                h_out = np.random.randint(1, 10)
+                w_out = np.random.randint(1, 10)
+                d_out = np.random.randint(1, 5)
+                #============================================
+
+            shape_in  = (h_in, w_in, d_in)
+            shape_out = (h_in, w_in, d_in)
+            shape_w   = (np.prod(shape_out), np.prod(shape_in) + 1)
 
             #================================================
             # network layer object to be tested ...
 
-            print("size_in={}, size_out={}".format(size_in, size_out))
+            print("shape_in={}, shape_out={}".format(shape_in, shape_out))
 
             layer = Dense(
-                size_in  = size_in,
-                size_out = size_out
+                shape_in  = shape_in,
+                shape_out = shape_out
             )
 
             #================================================
@@ -145,9 +225,9 @@ class TestDense(unittest.TestCase):
                     gy = np.random.normal(0.0, 1.0, shape_out)
 
                 # reference calculation ...
-                y_ref  = ref_forward     (x,  w, size_in, size_out)
-                gx_ref = ref_backward_gx (gy, w, size_in, size_out)
-                gw_ref = ref_backward_gw (gy, x, size_in, size_out)
+                y_ref  = ref_forward     (x,  w, shape_in, shape_out)
+                gx_ref = ref_backward_gx (gy, w, shape_in, shape_out)
+                gw_ref = ref_backward_gw (gy, x, shape_in, shape_out)
 
                 # set the layer weights according the reference values ...
                 layer.w = w
